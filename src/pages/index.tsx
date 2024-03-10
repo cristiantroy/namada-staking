b9c34cdca808f9ccac458795cff170422dec90d5
cristiantroy <154241727+cristiantroy@users.noreply.github.com>
2024-03-10 17:31:03 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Home | Namada Explorer</title>
+        <title>Namada Explorer</title>
         <meta name="description" content="Home | Namada Explorer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
