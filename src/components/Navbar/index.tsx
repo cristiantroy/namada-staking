e801d290f7d41e4d32dfa263b76b697e19c6744d
cristiantroy <154241727+cristiantroy@users.noreply.github.com>
2024-03-10 17:57:58 +0800

    black mode

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -46,7 +46,7 @@ export default function Navbar() {
 
   const [inputSearch, setInputSearch] = useState('')
   const [isLoadedSkeleton, setIsLoadedSkeleton] = useState(false)
-  toggleColorMode()
+  //toggleColorMode()
 
   useEffect(() => {
     if (tmClient) {
